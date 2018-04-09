82c4203b621a95881201cd335f1232dabd0172aa
raghuvanhi Ashutosh Kumar <uniqueraghuvanshi@gmail.com>
2018-04-09 15:03:49 +0530

    It is my first commit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+class Welcome
+{
+public static void main(String args[])
+{
+System.out.println("Hi there,you are welcome.!");
+System.out.println("Now This is your master branch,start coding");
+
+}
+
+}
